e5da22e8010628a3839f3e318d453f8cb5a8347c
ankit <ap8424856@gmail.com>
2024-04-24 17:35:02 +0530

    added contact route to root router

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
 router.use("/auth", require("./auth.route"));
-router.use("/user", require("./user.route"))
+router.use("/user", require("./user.route"));
+router.use("/contact", require("./contact.route"));
+
 
 module.exports = router;
