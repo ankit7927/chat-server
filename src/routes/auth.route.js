9d9cb165cc3ea58f3e1585988104516e289743bb
ankit <ap8424856@gmail.com>
2024-04-27 08:23:25 +0530

    fixed some issues

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,10 +2,7 @@ const authController = require("../controllers/auth.controller");
 
 const router = require("express").Router();
 
-router.route("/signin")
-    .post(authController.signin);
-
-router.route("/signup")
-    .post(authController.signup);
+router.route("/signin").post(authController.signin);
+router.route("/signup").post(authController.signup);
 
 module.exports = router;
